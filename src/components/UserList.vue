6e772a4c419a8d3aef802a955380bfa63230e1d6
kenu <kenu.heo@gmail.com>
2021-02-09 13:55:32 +0900

    axios get link

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -2,15 +2,30 @@
   <div>
     <h1>사용자 리스트</h1>
     <ul>
-      <li>하나</li>
-      <li>둘</li>
-      <li>셋</li>
+      <li v-for="item in list" :key="item">{{ item }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      list: ['하나', '둘', '셋'],
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:8083/api/users').then((res) => {
+      console.log(res.data);
+      const names = res.data.map((item) => {
+        return item.name;
+      });
+      this.list = names;
+    });
+  },
+};
 </script>
 
 <style></style>
